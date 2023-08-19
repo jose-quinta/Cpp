617fa413a521b2c037b4d47824266ff41541e607
Jose Quinta <jq1660824@gmail.com>
2023-08-19 17:49:58 -0600

    Explicacion de los tipos de datos primitivo en cpp

---
+++ TipoDatosPrimarios/AppTiposDatosPrimarios.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// Comienzo de explicacion de tipos de datos
+/*
+
+ Tipos de datos primitivos
+
+int: tipo de datos numerico 1, 2, 3, 4..
+float: tipo de dato numerico con punto decimal 1.1, 1.2, 1.3...
+double: tipo de dato numerico con punto decimal 1.02, 1.03....
+char: caracter unico a, b, c...
+bool: caracter booleano sea true o false, 1 o 0
+
+ Tipos de datos que venga de una libreria estandar
+
+string: es una cadena de caracteres
+vector: un contenedor de un tipo de dato especifico
+["Esta es la posicion 0", "pos 1", "pos 2"]
+
+*/
+// Final de explicacion
+
+int main() {
+	
+	
+	return 0;
+}
+
