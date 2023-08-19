d911de5bd74c194430c79e56ecac249640cc2f3b
Jose Quinta <jq1660824@gmail.com>
2023-08-19 17:51:38 -0600

    Como declarar una variable en cpp

---
+++ DeclararVariable/DeclararVariable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// Como definir una variable en cpp
+	int edad = 0;
+	
+	cout<<"Ingrese su edad: ";
+	cin>>edad;
+	
+	cout<<"La edad del usuario es de: "<< edad << endl;
+	cout<<"Programa finalizado!!!";
+	
+	return 0;
+}
